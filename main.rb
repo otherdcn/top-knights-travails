3c2e9ce3eb571ff55f67404ecd57c7fe1e15d2fb
otherdcn <danny.c.ntwari@gmail.com>
2024-09-11 12:30:48 +0200

    Add driver code in main.rb

---
+++ main.rb
@@ -0,0 +1,18 @@
+require_relative 'lib/knight'
+
+print "Enter start point (double digits from 0-7 seperated by comma): "
+start_coord = gets.chomp.split(",").map(&:to_i)
+
+print "Enter end point (double digits from 0-7 seperated by comma): "
+end_coord = gets.chomp.split(",").map(&:to_i)
+
+puts "\nStart at #{start_coord} and end at #{end_coord}\n"
+
+knight = Knight.new
+
+path = knight.knight_moves(start_coord, end_coord)
+
+puts "You made it in #{path.size - 1} moves!  Here's your path: "
+path.each do |square|
+  print "#{square.coord}\n"
+end
